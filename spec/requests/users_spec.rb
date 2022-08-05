b144dbc3bdc2aa3fb0d4790d3745529f95b85abe
Utsubo256 <greeeentea256@gmail.com>
2022-08-05 11:53:36 +0900

    Add edit action in users controller

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -54,4 +54,69 @@ RSpec.describe "Users", type: :request do
       end
     end
   end
+
+  describe "PATCH /users" do
+    let!(:user) { FactoryBot.create(:user) }
+
+    it "タイトルがEdit user | Ruby on Rails Tutorial Sample Appであること" do
+      get edit_user_path(user)
+      expect(response.body).to include full_title("Edit user")
+    end
+
+    context "in case of invalid value" do
+      it "更新できないこと" do
+        patch user_path(user), params: { user: { name: "",
+                                         email: "foo@invalid",
+                                         password: "foo",
+                                         password_confirmation: "bar" } }
+        user.reload
+        expect(user.name).to_not eq ""
+        expect(user.email).to_not eq ""
+        expect(user.password).to_not eq "foo"
+        expect(user.password_confirmation).to_not eq "bar"
+      end
+
+      it "更新アクション後にeditのページが表示されていること" do
+        get edit_user_path(user)
+        patch user_path(user), params: { user: { name: "",
+                                         email: "foo@invalid",
+                                         password: "foo",
+                                         password_confirmation: "bar" } }
+        expect(response.body).to include full_title("Edit user")
+      end
+
+      it "The form contains 4 errorsと表示されていること" do
+        patch user_path(user), params: { user: { name: "",
+                                                 email: "foo@invalid",
+                                                 password: "foo",
+                                                 password_confirmation: "bar" } }
+        expect(response.body).to include "The form contains 4 errors."
+      end
+    end
+
+    context "in case of valid value" do
+      before do
+        @name = "Foo Bar"
+        @email = "foo@bar.com"
+        patch user_path(user), params: { user: { name: @name,
+                                                 email: @email,
+                                                 password: "",
+                                                 password_confirmation: "" } }
+      end
+
+      it "更新できること" do
+        user.reload
+        expect(user.name).to eq @name
+        expect(user.email).to eq @email
+      end
+
+      it "Users#showにリダイレクトすること" do
+        expect(response).to redirect_to user
+      end
+
+      it "flashが表示されていること" do
+        expect(flash).to be_any
+      end
+    end
+  end
 end
